da6319ea0f724954fcec667c740dd36bb686e41b
Bruno Rodrigues <blich260@gmail.com>
2024-12-30 18:53:38 -0300

    Desafio das Cartas(teste 1)
    Descrição da questão proposta pelo teste:
    Questão #01 [Desafio das Cartas]
    Zeca e Chico acabaram de inventar um jogo de cartas para passar o tempo enquanto esperam pelo horário da
    escola. Cada um deles começa o jogo com uma pilha de 07 cartas viradas para baixo.
    A cada rodada, Zeca e Chico apresentam 01 carta para confronto, mas existe um detalhe importante…
    Zeca sempre pegará a carta que estiver no topo da pilha, enquanto Chico sempre pegará a carta que estiver na
    base da pilha.
    Deste modo, o valor das cartas apresentadas determinará o vencedor de cada rodada…
    ● Se a carta de Zeca for maior, ele é o VENCEDOR da rodada e descarta essa carta, enquanto Chico (que
    perdeu) devolve a carta ao seu monte e embaralha todas elas.
    ● Se a carta de Chico for maior, ele é o VENCEDOR da rodada e descarta essa carta, enquanto Zeca (que
    perdeu) devolve a carta ao seu monte e embaralha todas elas.
    ● Se ambas tiverem o mesmo valor, há um EMPATE, as duas cartas são descartadas e nenhum dos dois
    embaralha as cartas que restaram.
    Perceba que as cartas não mudam de uma rodada para outra! Somente são embaralhadas, e apenas pelo
    competidor que perder em cada rodada.
    O jogo termina apenas quando algum deles não tiver mais cartas, sagrando-se assim VENCEDOR DA PARTIDA.
    Ana, a primogênita da família estudante de BSI, assistindo aos irmãos brincarem, achou o jogo interessante e
    decidiu implementar uma versão informatizada do game para estudar para a Prova #01 de ED-1. Assim…
    PLAYER 1 e PLAYER 2 receberão, cada um, 07 cartas aleatórias sorteadas no intervalo entre 1 e 10 (inclusive).
    O PLAYER 1 sempre apresentará a carta do topo do baralho.
    O PLAYER 2 sempre apresentará a carta da base do baralho.
    Para esta implementação, considere que o índice 0 do Array corresponda ao topo do baralho.
    A implementação do game deve mostrar o confronto das cartas e o resultado a cada rodada.
    Também será necessário revelar o valor e a evolução das cartas de cada jogador, a fim de verificar a corretude
    do software.

---
+++ Teste.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h> 
+#include <time.h> 
+
+int embaralha(int cartas[7], int qual, int ultC, int ultZ) {
+    int alea=rand()%4 + 2;
+    while(alea){
+        if(qual==1)
+            for(int i=0; i<ultC; i++){
+                int prov=0, ind=0;
+                prov=cartas[i]; 
+                ind=rand()%(ultC+1);
+                cartas[i] = cartas[ind]; 
+                cartas[ind] = prov; 
+            }
+        if(qual==2)
+            for(int i=ultZ; i<6; i++){
+                int prov=0, ind=0;
+                prov=cartas[i]; 
+                ind=rand()%(7-ultZ); 
+                cartas[i] = cartas[ind]; 
+                cartas[ind] = prov; 
+            }
+        alea--;
+    }
+    return cartas[7]; 
+}
+
+int main () {
+    int cartasz[7], cartasc[7], validac[7], validaz[7], mostraZ=0, mostraC=0; 
+    int ultC=6; 
+    int ultZ=0, fim=0;
+    srand(time(NULL)); 
+
+    //Colocando valores de cartas aleatórios nas cartas de zeca e chico e valida como 1
+    for(int i=0; i<7; i++){
+        cartasz[i] = rand()%10 + 1; 
+        validac[i] = 1; 
+        validaz[i] = 1; 
+        cartasc[i] = rand()%10 + 1; 
+    }
+
+    //Desenhar as cartas de zeca e chico (viradas de cabeça pra baixo)
+    printf("Cartas Zeca: "); 
+   for(int i=0; i<7; i++) 
+        printf("[] ");
+    printf("\n\n\n\n\n"); 
+    printf("Cartas Chico: "); 
+    for(int i=0; i<7; i++) 
+        printf("[] "); 
+
+
+    //Repetição principal
+    do{
+        printf("\nAperte ENTER para continuar...\n"); 
+        getchar(); 
+
+        //Virando as cartas Zeca
+        printf("Cartas Zeca: ");
+        for(int i=0; i<7; i++){
+            if(ultZ==i){
+                printf("[%02d] ", cartasz[i]);
+                mostraZ = cartasz[i];   
+                continue; 
+            }
+            if(validaz[i]){
+                printf("[] "); 
+            }else{
+                printf("X "); 
+            }
+        }
+        printf("\n\n\n\n");
+        //Virando as cartas Chico
+        printf("Cartas Chico: ");
+        for(int i=0; i<7; i++){
+            if(ultC==i){
+                printf("[%02d] ", cartasc[i]); 
+                mostraC = cartasc[i];    
+                continue; 
+            }
+            if(validac[i]){
+                printf("[] "); 
+            }else{
+                printf("X "); 
+            }
+        }
+
+        //Comparando quem venceu e fazendo as coisas 
+        printf("\nZeca: %d vs Chico: %d", mostraZ, mostraC); 
+        if(mostraZ>mostraC){
+            printf("\nZeca Venceu a rodada!\n"); 
+            validaz[ultZ]=0; 
+            ultZ++;
+            embaralha(cartasc, 1, ultC, ultZ); 
+        }else{
+            if(mostraZ==mostraC){
+                printf("\nDeu empate!"); 
+                validac[ultC]=0;
+                validaz[ultZ]=0; 
+                ultC--; 
+                ultZ++;
+            }else{
+                printf("\nChico Venceu a rodada!\n");
+                validac[ultC]=0; 
+                ultC--; 
+                embaralha(cartasz, 2, ultC, ultZ); 
+            }
+        }
+        if(ultC<0 && ultZ>6){
+            printf("O jogo acabou em empate!"); 
+            break; 
+        }
+
+        if(ultC<0){
+            printf("Chico ganhou o jogo!"); 
+            break;
+        } 
+        if(ultZ>6){
+            printf("Zeca ganhou o jogo!"); 
+            break;
+        }
+
+    }while(1); 
+
+
+    return 0;
+}
