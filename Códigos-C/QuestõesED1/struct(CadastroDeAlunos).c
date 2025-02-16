d34bb40718a486b5f938279f66287a90dcdd8e64
Bruno Rodrigues <blich260@gmail.com>
2025-02-16 15:15:03 -0300

    Add files via upload

--- Códigos-C/QuestõesED1/struct(CadastroDeAlunos).c
+++ Códigos-C/QuestõesED1/struct(CadastroDeAlunos).c
@@ -1,6 +1,21 @@
 /*
-  Ta faltando no preenchimento normal: 
-    -Parte 2 e 3 do problema;
+1. Faça um programa que define um novo tipo de dados chamado Aluno. Cada 
+registro de Aluno deve conter: Nome do Estudante (s), Número de Matricula (i), 
+Nome do Curso (s), Média de Notas (f).
+ a)
+ Leia os dados de vários alunos (até o nome informado for “exit”).
+ b)
+ c)
+ Após a fase de cadastro, pergunte ao usuário do sistema algum Número 
+de Matrícula para ser pesquisado, e encontrando o registro, imprima 
+todas as informações deste aluno.
+ Repita a operação da letra B acima, até que o usuário informe um Nº de 
+matrícula negativo (para encerrar o programa).
+ 2. Refatore o problema anterior. Agora, toda vez que um aluno for pesquisado, 
+deve-se perguntar se o usuário deseja lançar uma nota para este aluno. Sabe-se 
+que todo aluno pode ter até 10 notas. 
+Todas as notas individuais devem ser salvas, e a Média de Notas deve ser 
+recalculada automaticamente pelo sistema.
 */
 #include <stdio.h>
 #include <stdlib.h>
